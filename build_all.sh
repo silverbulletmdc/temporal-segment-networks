d5cb3ee9e5648fe70467165d1a5ae322a43d0e0d
dechao_meng <mengdechaolive@qq.com>
2018-11-26 09:35:44 +0800

    Change to OpenCV3.4

--- build_all.sh
+++ build_all.sh
@@ -15,12 +15,15 @@ sudo apt-get -qq install libzip-dev
 
 # install common dependencies: OpenCV
 # adpated from OpenCV.sh
-version="2.4.13"
+version="3.4.4"
 
 echo "Building OpenCV" $version
 [[ -d 3rd-party ]] || mkdir 3rd-party/
 cd 3rd-party/
-
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.4
+cd ..
 if [ ! -d "opencv-$version" ]; then
     echo "Installing OpenCV Dependenices"
     sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
@@ -36,7 +39,7 @@ echo "Building OpenCV" $version
 cd opencv-$version
 [[ -d build ]] || mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_TBB=ON  -D WITH_V4L=ON ..
+cmake -D WITH_CUDA=ON -DWITH_OPENGL=ON -D OPENCV_ENABLE_NONFREE=ON -D CMAKE_BUILD_TYPE=RELEASE -D WITH_TBB=ON  -D WITH_V4L=ON -D BUILD_TIFF=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 if make -j32 ; then
     cp lib/cv2.so ../../../
     echo "OpenCV" $version "built."
@@ -50,6 +53,8 @@ cd ../../../
 
 echo "Building Dense Flow"
 cd lib/dense_flow
+git stash
+git checkout opencv-3.1
 [[ -d build ]] || mkdir build
 cd build
 OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
