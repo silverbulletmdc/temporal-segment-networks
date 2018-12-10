d359d6d24a8019bb8871353881338fff4958a0f0
Dechao Meng <dechao_meng@qq.com>
2018-12-10 14:16:00 +0800

    Fix some bug in OpenCV3

--- tools/build_of.py
+++ tools/build_of.py
@@ -48,8 +48,7 @@ def run_optical_flow(vid_item, dev_id=0):
     image_path = '{}/img'.format(out_full_path)
     flow_x_path = '{}/flow_x'.format(out_full_path)
     flow_y_path = '{}/flow_y'.format(out_full_path)
-
-    cmd = os.path.join(df_path + 'build/extract_gpu')+' -f {} -x {} -y {} -i {} -b 20 -t 1 -d {} -s 1 -o {} -w {} -h {}'.format(
+    cmd = os.path.join(df_path + 'build/extract_gpu')+' -f={} -x={} -y={} -i={} -b=20 -t=1 -d={} -s=1 -o={} -w={} -h={}'.format(
         quote(vid_path), quote(flow_x_path), quote(flow_y_path), quote(image_path), dev_id, out_format, new_size[0], new_size[1])
 
     os.system(cmd)
